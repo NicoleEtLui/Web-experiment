8a19ea106f724d355bd4b1060af13ce82b04b187
NicoleEtLui <petit.martin125@gmail.com>
2017-05-27 11:00:34 +0200

    drawing with click works

--- textPaint/JS/textPaint.js
+++ textPaint/JS/textPaint.js
@@ -1,27 +1,40 @@
 var canvasHeight, canvasWidth, drawingTable; 
 
 var p5Instance = function (p) {
-    var canvas, str;
+    var canvas, symbol, fontRegular;
+    
     
     str = "";
+   
+    
+    function drawCanvas () {
+        p.fill(255, 255, 255);
+        p.stroke(255);
+        p.textSize(16);
+        for (var i = 0, k = 0; i < canvasWidth; i++){
+            for (var j = 0; j < canvasHeight; j++) {
+                //console.log(drawingTable[i][j]);
+                symbol = drawingTable[i][j];
+                p.text(symbol, 3 + (i * 16), 15 + (j * 16)); 
+            }
+        }
+    };
+    
+    /* p.preload = function () {
+        fontRegular = p.loadFont("FONT/SourceCodePro-Regular.otf");
+    }*/
     p.setup = function () {
+        p.frameRate(10);
         canvas = p.createCanvas(canvasHeight * 16, canvasWidth * 16);
         canvas.parent('canvasContainer');
         canvas.id('drawingCanvas');
-        
+        //p.textFont('Courier New');
     };
 
     p.draw = function () {
         p.background(0, 0, 0);
-        p.fill(255, 255, 255);
+        drawCanvas();
         
-        p.noStroke();
-        p.textSize(22);
-        str = '+';
-        p.text(str, 2, 16);
-        p.text(str, 18, 16);
-        p.text(str, 2, 32);
-        p.text(str, 18, 32);
         p.stroke(255);
         p.line(0, 16, 32, 16);
         p.line(16, 0, 16, 32);
@@ -30,18 +43,41 @@ var p5Instance = function (p) {
 
 };
 
-$(function() {
-    drawingTable = [""];
-    canvasHeight = 2;
-    canvasWidth = 2;
+function initCanvas() {
+    $('#ink').val('/');
+    $('#canvasHeight').val(2);
+    $('#canvasWidth').val(2); 
+    canvasHeight = $('#canvasHeight').val();
+    canvasWidth = $('#canvasWidth').val();
+    drawingTable = [["0"," "],[" "," "]];
     
-    for (var i = 0; i < canvasWidth * canvasHeight; i++) {
-        drawingTable[i] = "+";
-    };
+};
+
+/*function updateCanvas() {
+    ink = $('#ink').val(); 
+    canvasHeight = $('#canvasHeight').val();
+    canvasWidth = $('#canvasWidth').val();
+};*/
+
+$(function() {
+    initCanvas();
+    
     
     myp5 = new p5(p5Instance);
     
     $(document).keydown(function(event){
         $('#ink').val(event.key);
     });
+    
+    $('#drawingCanvas').click(function (e) {
+        var offset = $(this).offset(); // permet de récupérer un objet contenant le décalage du canvas par rapport au document
+        var relMouseX = e.pageX - offset.left; // translation du (0,0) du document sur le coin supérieur gauche du canvas
+        var relMouseY = e.pageY - offset.top;
+        
+        var cellX = ~~(relMouseX/16);
+        var cellY = ~~(relMouseY/16);
+        drawingTable[cellX][cellY] = $('#ink').val();
+        
+    });
+    
 });
